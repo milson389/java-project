fe32bc4611a8b506f1bffa6400f1411292356f80
milson389 <mugetsu3008@gmail.com>
2020-07-16 07:08:44 +0700

    User input dengan InputStreamReader, dan BufferedReader

--- JavaFundamental/src/com/dicoding/javafundamental/inputoutput/InputOutputFunction.java
+++ JavaFundamental/src/com/dicoding/javafundamental/inputoutput/InputOutputFunction.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputOutputFunction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Pemrograman penjumlahan sangat sederhana");
+        System.out.println("Program penjumlahan sangat sederhana");
         System.out.print("Masukkan angka pertama : ");
         int value1 = scanner.nextInt();
         System.out.print("Masukkan angka kedua : ");
