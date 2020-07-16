6d6fdc9344190bed4fae36ab5c3171985a0a48e4
milson389 <mugetsu3008@gmail.com>
2020-07-17 00:06:21 +0700

    Example Split Calendar

--- JavaFundamental/src/com/dicoding/javafundamental/datetime/ExampleSystemCurrentMilis.java
+++ JavaFundamental/src/com/dicoding/javafundamental/datetime/ExampleSystemCurrentMilis.java
@@ -1,8 +1,19 @@
 package com.dicoding.javafundamental.datetime;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class ExampleSystemCurrentMilis {
     public static void main(String[] args) {
         long timeNow = System.currentTimeMillis();
         System.out.println("Waktu sekarang adalah " + timeNow + " milisecond");
+
+        Date date = new Date();
+        System.out.println("Tanggal sekarang adalah " + date.toString());
+
+        // Menampilkan waktu sekarang
+        Calendar calendar = Calendar.getInstance();
+        System.out.println("Waktu sekarang adalah " + calendar.getTime());
+
     }
 }
