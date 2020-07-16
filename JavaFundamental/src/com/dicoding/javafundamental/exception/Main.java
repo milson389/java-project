d8641c731681be631e63297107a97744e1bdfdc5
milson389 <mugetsu3008@gmail.com>
2020-07-16 23:36:51 +0700

    Exception

---
+++ JavaFundamental/src/com/dicoding/javafundamental/exception/Main.java
@@ -0,0 +1,26 @@
+package com.dicoding.javafundamental.exception;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            File file = new File("D://Recovery.txt");
+            FileReader fr = new FileReader(file);
+            System.out.println("Read file berhasil");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        int[] a = new int[5];
+        try {
+            System.out.println("Akses elemen ke 5 :" + a[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown :" + e);
+        } finally {
+            a[4] = 10;
+            System.out.println("Nilai elemen terakhir: " + a[4]);
+        }
+    }
+}
