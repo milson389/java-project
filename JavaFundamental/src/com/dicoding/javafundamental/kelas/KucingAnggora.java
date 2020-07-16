9f682694d763fcee103cbe8af2d469f9ac291133
milson389 <mugetsu3008@gmail.com>
2020-07-16 19:08:52 +0700

    Java creating instance/ class

---
+++ JavaFundamental/src/com/dicoding/javafundamental/kelas/KucingAnggora.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.kelas;
+
+public class KucingAnggora {
+    String nama_kucing;
+    int berat_badan;
+
+    public KucingAnggora( String nama, int berat){
+        this.nama_kucing = nama;
+        this.berat_badan = berat;
+    }
+}
